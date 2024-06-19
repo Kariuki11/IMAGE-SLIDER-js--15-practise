93818bc1ccae43bd5384c1ad974bbe7c1513fa88
Kariuki11 <irungukenneth433@gmail.com>
2024-06-20 02:27:34 +0300

    New changes

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let slideIntervalId = null;
 
-initializeSlider();
+// initializeSlider();
+document.addEventListener("DOMContentLoaded", initializeSlider)
 
 function initializeSlider(){
 
     slides[slideIndex].classList.add("displaySlide")
-
+    IntervalId = setInterval(nextSlide, 5000);
+    console.log(IntervalId);
 }
 function showSlide(index){
 
